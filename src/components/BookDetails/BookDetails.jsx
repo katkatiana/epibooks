9251a30405053fcc0ac01c2c139dcd7f54a6059f
Katia Santangelo <msantangelo56@gmail.com>
2024-03-04 18:54:24 +0100

    Forgot import of css

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -14,6 +14,7 @@ import { useParams } from 'react-router-dom';
 import { useContext } from 'react';
 import { BookContext } from '../../contexts/BookContext';
 import MyCommentArea from '../CommentArea/MyCommentArea';
+import "./BookDetails.css"
 
 /******** Internal Variables  ***************************************************/
 
